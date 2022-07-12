42a94ce519589bf3657dd72baead47c106c01f3b
lamekammi <kameronnb@gmail.com>
2022-07-12 08:42:01 -0700

    adding index.html, style.css, and adding class Figure

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Figure:
+    def __init__(self, name, company, release, description):
+        self.name = name
+        self.company = company
+        self.release = release
+        self.description = description
+
+figures = [
+    Figure(),
+    Figure(),
+    Figure()
+]
+
 
 # Create your views here.
 def home(request):
@@ -8,3 +21,6 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def figures_index(request):
+    return render(request, 'figures/index.html', { 'figures': figures })
