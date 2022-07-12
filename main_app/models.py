7d8be8734bc76d4edf5d414fb959a9d623a27417
lamekammi <kameronnb@gmail.com>
2022-07-12 12:20:03 -0700

    making a model in models.py and adding migrations

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Figure(models.Model):
+    name = models.CharField(max_length=100)
+    company = models.CharField(max_length=100)
+    release = models.CharField(max_length=100)
+    price = models.IntegerField()
+    decription = models.TextField(max_length=250)
--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-class Figure:
-    def __init__(self, name, company, release, description):
-        self.name = name
-        self.company = company
-        self.release = release
-        self.description = description
-
-figures = [
-    Figure('Raphtalia', 'KADOKAWA', 'unknown', 'Pop Up Parade of Raphtalia from rising of the shield hero'),
-    Figure('Tohru', 'APEX', '11-01-2022', 'Tohru in her work uniform from season2'),
-    Figure('Levi Ackerman', 'Kotobukiya', 'unknwon', 'levi on a tree mid swing')
-]
-
 
 # Create your views here.
 def home(request):
