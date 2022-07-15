63ad24bbc56ac1e4cb9b5ee927d3603d58e65b63
lamekammi <kameronnb@gmail.com>
2022-07-15 00:26:38 -0700

    adding Comment model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Figure
+from .models import Figure, Comment
 
 # Register your models here.
 admin.site.register(Figure)
+admin.site.register(Comment)
--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.urls import reverse
 
+
+RATINGS = (
+    (1, 1),
+    (2, 2),
+    (3, 3),
+    (4, 4),
+    (5, 5)
+)
 # Create your models here.
 class Figure(models.Model):
     name = models.CharField(max_length=100)
@@ -14,3 +22,16 @@ class Figure(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'figure_id': self.id})
+
+class Comment(models.Model):
+    rating = models.IntegerField(
+        choices=RATINGS,
+        default=RATINGS[0][0]
+    )
+    comment = models.TextField(max_length=250)
+
+    figure = models.ForeignKey(Figure, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Rating: {self.get_rating_display()} Comment: {self.comment}"
+
