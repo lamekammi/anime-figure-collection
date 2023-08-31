81b92963f3fa4f414819488124c83a2bf09a536b
lamekammi <kameronnb@gmail.com>
2023-08-31 12:08:11 -0700

    static update5

--- afcollection/urls.py
+++ afcollection/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.staticfiles import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main_app.urls')),
     path('about/', include('main_app.urls')),
-]
-urlpatterns += staticfiles_urlpatterns()
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
